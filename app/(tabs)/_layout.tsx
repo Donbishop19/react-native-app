0fc33d3467086d091faae761718561111aceb9ac
Busayo Tella <114032372+Donbishop19@users.noreply.github.com>
2026-04-08 09:53:58 -0700

    implement fonts

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import {tabs} from "@/constants/data";
 import {View} from "react-native";
 import  { colors, components } from "@/constants/theme";
 import clsx from "clsx";
-import {Image} from "react-native";
+import {Image} from "expo-image";
 import  { useSafeAreaInsets } from "react-native-safe-area-context";
 
 
